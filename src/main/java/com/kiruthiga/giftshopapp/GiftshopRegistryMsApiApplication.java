3f9aba4f076a0d4cd9ca70aadb4bf8235eadfa55
kiruthiga2408 <keerthilalli24@gmail.com>
2022-05-05 16:51:38 +0530

    added

---
+++ src/main/java/com/kiruthiga/giftshopapp/GiftshopRegistryMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.kiruthiga.giftshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftshopRegistryMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftshopRegistryMsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiruthiga/giftshopapp/GiftshopRegistryMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiruthiga.giftshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftshopRegistryMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
